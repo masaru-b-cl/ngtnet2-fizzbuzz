af2891d63f95b2ea60c88e643b335d0ca14159bd
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2016-05-07 02:18:05 +0900

    first test

---
+++ FizzBuzz.Test/FizzBuzzTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.Test
+{
+  [TestClass]
+  public class FizzBuzzTest
+  {
+    [TestMethod]
+    public void 引数が1の場合文字列で1を返す()
+    {
+      // arrange
+      var fizzBuzzer = new FizzBuzzer();
+
+      // act
+      string result = fizzBuzzer.Say(number: 1);
+
+      // assert
+      result.Is("1");
+    }
+  }
+}
