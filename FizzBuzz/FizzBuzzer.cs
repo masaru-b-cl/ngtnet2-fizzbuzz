b89706c18621ab0302e3fe524539281e0a4cdd39
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2016-05-07 02:59:00 +0900

    fake it

---
+++ FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+  public class FizzBuzzer
+  {
+    public FizzBuzzer()
+    {
+    }
+
+    public string Say(int number)
+    {
+      return "1";
+    }
+  }
+}
