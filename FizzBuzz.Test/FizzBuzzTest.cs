7f7fd5f2751c0ae2c7651197530b4e0a2370cf3a
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2016-05-07 03:11:27 +0900

    introduce setup

--- FizzBuzz.Test/FizzBuzzTest.cs
+++ FizzBuzz.Test/FizzBuzzTest.cs
@@ -6,19 +6,23 @@ namespace FizzBuzz.Test
   [TestClass]
   public class FizzBuzzTest
   {
+    private FizzBuzzer fizzBuzzer;
+
+    [TestInitialize]
+    public void SetUp()
+    {
+      fizzBuzzer = new FizzBuzzer();
+    }
+
     [TestMethod]
     public void 引数が1の場合文字列で1を返す()
     {
-      var fizzBuzzer = new FizzBuzzer();
-
       fizzBuzzer.Say(1).Is("1");
     }
 
     [TestMethod]
     public void 引数が2の場合文字列で2を返す()
     {
-      var fizzBuzzer = new FizzBuzzer();
-
       fizzBuzzer.Say(2).Is("2");
     }
   }
